be6f7e60e55b58e547937aa9e7e1020d0ba41f6c
moodyprep <ryan@moodyprep.com>
2024-11-13 16:08:45 -0500

    removed imports that seemed to be causing error in vercel build

--- server/api/add/sat-skill.js
+++ server/api/add/sat-skill.js
@@ -1,5 +1,5 @@
 import { serverSupabaseServiceRole } from '#supabase/server'
-import { prepareSATSkillForDB } from '~/assets/composables/prepareSATSkillForDB';
+//import { prepareSATSkillForDB } from '~/assets/composables/prepareSATSkillForDB';
 
 
 
@@ -10,12 +10,13 @@ export default eventHandler(async (event) => {
     console.log('adding skill');
     const client = serverSupabaseServiceRole(event)
     const body = await readBody(event);
+    /*
     const sb_data = prepareSATSkillForDB(body);
     
     console.log('sb_data', sb_data);
     const sb_resp = await client.from('sat_skills').insert(sb_data);
     return { data: sb_resp.data, error: sb_resp.error };
-
+    */
 
 
 })
--- server/api/edit/sat-skill/[id].js
+++ server/api/edit/sat-skill/[id].js
@@ -1,5 +1,5 @@
 import { serverSupabaseServiceRole } from '#supabase/server'
-import { prepareSATSkillForDB } from '~/assets/composables/prepareSATSkillForDB';
+//import { prepareSATSkillForDB } from '~/assets/composables/prepareSATSkillForDB';
 
 
 
@@ -11,11 +11,12 @@ export default eventHandler(async (event) => {
     console.log('adding problem');
     const client = serverSupabaseServiceRole(event)
     const body = await readBody(event);
+    /*
     const sb_data = prepareSATSkillForDB(body);
     const sb_resp = await client.from('sat_skills').update(sb_data).eq('id', skill_id);
     console.log('sb_data', sb_data);
     return { data: sb_resp.data, error: sb_resp.error };
-
+    */
 
 
 })
