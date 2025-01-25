e266a9e9afc3cd278e2c01e1def0338615438fa5
rmoody09 <ryan.moody09@gmail.com>
2025-01-25 09:46:06 -0500

    more content on reading writing

--- pages/sat/reading-writing/standard-english/question-types/verb-form.vue
+++ pages/sat/reading-writing/standard-english/question-types/verb-form.vue
@@ -100,7 +100,7 @@
                     </li>
                 </ul>
             </div>
-            <div class='font-semibold pt-6 pb-1'>Gerunds (when an "ing" verb acts as a noun)</div>
+            <div class='font-semibold pt-6 pb-1'>Can act as a noun (called a gerund)</div>
             <div>
                 When an "ing" verb acts as a noun, it's called a gerund. For example, in the sentence "Sleeping is the cat's favorite activity", "sleeping" is used as noun that represents the activity of sleeping. This sentence is complete because  "sleeping" is the subject of the sentence and the main verb is "is". Here are a few more examples of gerunds:
                 <ul class='pt-2 pl-8'>
@@ -115,7 +115,7 @@
                     </li>
                 </ul>
             </div>
-            <div class='font-semibold pt-6 pb-1'>Present Participles (when an "ing" verb acts as an adjective)</div>
+            <div class='font-semibold pt-6 pb-1'>Can act as an adjective (called a present participle)</div>
             <div>
                 "ing" verbs can also be used as an adjective, known as a present participle. For example, the phrase "The sleeping cat" is not a complete sentence because "sleeping" is not used as a verb; it is used as an adjective  to describe the state of the cat. We could make the sentence complete by adding a main verb, like "The sleeping cat was woken by the squeaking mouse." Here are a few more examples of present participles:
                 <ul class='pt-2 pl-8'>
@@ -130,9 +130,34 @@
                     </li>
                 </ul>
             </div>
-            <div class="font-semibold pt-5 pb-1">"ing" verbs can begin non-essential clauses</div>
+            <div class="font-semibold pt-5 pb-1">"-ing" verbs can begin non-essential or essential clauses</div>
             <div>
-                "Ing" verbs can also be used to lead off non-essential clauses, as we saw in the earlier section. For example, "The cat, <b>hoping to finally catch its prey</b>, pounced on the red dot." In that sentence, "hoping to finally catch its prey" is a non-essential clause that is separated from the rest of the sentence by commas, so "hoping" is not a main verb. These non-essential clauses might appear in the middle of a sentence, like we just saw, or they can also be at the beginning of a sentence ("Hoping to finally catch its prey, the cat pounced on the red dot."), or at the end of a sentence ("The cat pounced on the red dot, hoping to finally catch its prey.").
+                "-ing" verbs can also be used to lead off essential or non-essential clauses. In these cases, the "ing" verb is acting as an adjective to either give more information about a noun in the sentence (for non-essential clauses), or specify the noun or pronoun that referred to (for essential clauses). 
+                <div class='pt-2'>
+                    Here are some examples of "ing" verbs used to lead off non-essential clauses. These require commas or em dashes to separate the non-essential clauses.
+                </div>
+                <ul class='pt-2 pl-8'>
+                    <li>
+                        "The cat —<b>hoping to finally catch its prey</b>— pounced on the red dot."
+                    </li>
+                    <li>
+                        "<b>Hoping to finally catch its prey</b>, the cat pounced on the red dot."
+                    </li>
+                    <li>
+                        "The cat pounced on the red dot, <b>hoping to finally catch its prey</b>."
+                    </li>
+                </ul>
+                <div class="pt-2">
+                    And here are some examples of "ing" verbs used to lead off essential clauses:
+                </div>
+                <ul class='pt-2 pl-8'>
+                    <li>
+                        "The student studying hardest will probably perform best on the test."
+                    </li>
+                    <li>
+                        "Most of the dogs were adorable, but the one barking his head off was annoying."
+                    </li>
+                </ul>
             </div>
         </div>
         <div class="font-bold text-lg pt-8 pb-2">
@@ -140,6 +165,65 @@
         </div>
         <div>
             When you add the word "to" in front of a verb, it becomes what's known as an infinitive verb, which does not act as a main verb. Depending on the context, it may act as a noun, adjective, or adverb. For example, in the sentence "To err is human", "to err" is an infinitive verb that acts as a noun, representing the act of erring. Also, in the sentence "I need to win", "to win" acts as a noun because it represents a thing that is needed. In the sentence "I went to buy bananas", "to buy" acts as an adverb because it describes the reason the subject "went". An infinitive verb can also act as an adjective, describing a noun. For example, in the sentence "The book to read is on the shelf", "to read" acts as an adjective describing the book.
+        </div>
+
+        <div class="font-bold text-lg pt-8 pb-2">
+            "-ed" Verbs Can Sometimes Act as Adjectives
+        </div>
+        <div>
+            When a verb ends in "-ed", it can sometimes act as an adjective. It might be a straightforward adjective placed right before the noun it describes, or it might begin a non-essential or essential clause. Here are some examples:
+            <ul class='pt-2 pl-8'>
+                <li>
+                    "The deflated balloon no longer floats."
+                    <ul>
+                        <li>
+                            "deflated" is an adjective describing the state of the balloon
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    "The bride, dressed in a beautiful white dress, walked down the aisle."
+                    <ul>
+                        <li>
+                            "dressed" leads off a non-essential clause adding more information about the bride
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    "I'm not letting the dog covered in mud in the house."
+                    <ul>
+                        <li>
+                            "covered" leads off the essential clause "covered in mud", which specifies which dog is being referred to
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+        <div class="font-bold text-lg pt-8 pb-2">
+            Determine which choice plays the proper role
+        </div>
+        <div>
+            On these types of questions, generally you will be able to intuitively tell which answer choice sounds right in the sentence. But hopefully the examples above will help you understand how to check for the different roles that a verb can play in its different forms. One thing to check for is if the sentence already has a main verb. If not, then the answer choice should have the verb in a form that can act as a main verb. Even if it does have a main verb already, sometimes a subject can perform multiple actions in a sentence (e.g. "The boxer bobbed and weaved to avoid his opponent's punches"), so don't jump right to assuming the blank can't act as a main verb.
+        </div>
+    </div>
+
+    <h2 class="sat-guide-section-header">Subject-verb agreement</h2>
+    <div class="section-description">
+        If you don't see answer choices with infinitives or "ing" verbs without a helper verb, then the next thing to check for is subject-verb agreement. Subject-verb agreement is the concept that the subject of a sentence and the verb must agree in number. If the subject is singular, then the verb should also be in singular form. If the subject is plural, then the verb should be in plural form. For example, it would be improper to say "The cats sleeps." While the error in this example seems obvious, on the SAT you'll encounter much more complex sentences where the subject and verb aren't right next to each other, so the "ear test" (going with the option that sounds right) doesn't always make the right answer obvious.
+
+        <div class="font-bold text-lg pt-8 pb-2">
+            Start with the "it" test
+        </div>
+        <div>
+            If the question is testing you on subject-verb agreement, then either 3 answer choices will use plural forms of the verb and one will be singular, or 3 answer choices will use singular forms of the verb and one will be plural. An easy way to check if this is the case is to plug in a singular pronoun before the verb in each answer choice (I generally like to use "it") and see it sounds right. If plugging in "it" sounds right, then the verb is singular. If plugging in "it" sounds wrong, then the verb is plural. So, if you perform the "it" test and find that there is a single outlier (either there is only one singular verb or there are exactly 3 singular verbs), then there's a good chance that the question is testing you on subject-verb agreement, and that the outlier is the right answer. Still, I would double check by identifying the subject and making sure it does indeed match the verb. If you don't find a single outlier, then the question must be testing for verb tense rather than subject-verb agreement, so you can move on to checking for verb tense.
+        </div>
+
+        <div class="font-bold text-lg pt-8 pb-2">
+            Check the subject
+        </div>
+        <div>
+            If the "it" test gave you a single outlier, if you're pressed for time then you might want to just select the outlier and move one, but it's generally good practice to double check by identifying the subject and making sure it matches the verb. Look at all the nouns earlier in the passage, and ask yourself if that is the specific noun that would be performing the verb in the answer choice.
         </div>
     </div>
 </template>
