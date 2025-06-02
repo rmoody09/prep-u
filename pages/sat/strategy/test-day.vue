daacadb7be5f3437f28d57a3ec0832b8bd2718a5
rmoody09 <ryan.moody09@gmail.com>
2025-06-02 09:59:19 -0400

    added content to test day/week strategy

--- pages/sat/strategy/test-day.vue
+++ pages/sat/strategy/test-day.vue
@@ -20,34 +20,39 @@
         Because you've already put in the work needed to do well, the best thing you can do is to make sure you are alert and focused on test day. Getting a good night's sleep not just the night before, but for a couple of nights in a row is crucial to making sure you are at full mental capacity on test day.
     </div>
 
-    <h2 class="sat-guide-section-header">Do Something That Relaxes You</h2>
+    <h2 class="sat-guide-section-header">Do Something That Relaxes You the Day/Night Before</h2>
     <div class="section-description">
-        
+        The night before the test, it's natural to be thinking about the test and maybe even be a little nervous about it. While doing some review the day before is perfectly fine and probably a good idea, it's also important to do something that relaxes you and takes your mind off the test for some of the day/evening. This could be anything from going for a walk, to watching a movie, to reading a book, to doing some yoga. Just make sure it's something that you enjoy and that you can do without stressing about the test. Especially if you've already done a lot of prep, you're better off making sure you go into the test in a good headspace and well rested than trying to put every ounce of energy into preparing for and thinking about the test.
     </div>
 
     <h2 class="sat-guide-section-header">Now Is Not The Time To Try Something New</h2>
     <div class="section-description">
-        
+        Test day is not the time to be experimenting with new things. For example, if you normally do the math problems in order from 1 to 22, don't try starting with the more difficult final problems on test day. Any experimentation should be done in practice, and only applied on test day if you've found that it helps you. This doesn't only apply to test strategies, but also to the way you eat, sleep, exercize, etc. For example, if you don't usually drink coffee, don't try drinking it the morning of the test to try to get an energy boost. And if you don't regularly go for long runs, don't go for a long run the day before the test.
     </div>
 
     <h2 class="sat-guide-section-header">Wear Layers</h2>
     <div class="section-description">
-        
+        It's important to be comfortable when you're taking the test. You never know what the temperature will be like in the testing room, so it's best to be prepared for anything. Bring layers so you can adjust to the temperature as needed.
     </div>
 
     <h2 class="sat-guide-section-header">Be Confident</h2>
     <div class="section-description">
-        
+        Studies have found again and again that simply believing you will do well on the test can lead to better performance. You've put in the work, you've prepared, and you've practiced. You've done everything you can to prepare for the test. Now, you just need to believe in yourself and go into the test with confidence.
     </div>
 
     <h2 class="sat-guide-section-header">You Can Always Take It Again (hopefully)</h2>
     <div class="section-description">
-        
+        Some level of test day anxiety is almost inevitable for everyone, but it's important to not let it affect your performance. Most likely, this is not your last possible opportunity to take the test, so remember that it's not the end of the world if you don't do as well as you'd like. Especially with superscoring being the norm at most colleges, your performance on this particular is probably not crucial to your success in getting into college. It's just an opportunity to hopefully put up a decent score that you can improve on with future tests if you need to.
     </div>
 
     <h2 class="sat-guide-section-header">If This Isn't Your First Time</h2>
     <div class="section-description">
-        
+        If you've already taken the test at least once, then just improving on one section will improve your superscore and should be considered a win. Even if you don't improve your score at all, chancess are your existing score will already get you into plenty of colleges. Think of this as an opportunity where you have nothing to lose, but a good chance to gain a score improvement
+    </div>
+
+    <h2 class="sat-guide-section-header">Leave Early</h2>
+    <div class="section-description">
+        You don't want to add any unnecessary stress to your test day. So leaving a little earlier than you think you need to is worth it, in case of unexpected traffic or other variables. Having to get up a few minutes earlier is better than being late, or even just having the stress of feeling like you might be late.
     </div>
 
     <h2 class="sat-guide-section-header">Be Prepared</h2>
